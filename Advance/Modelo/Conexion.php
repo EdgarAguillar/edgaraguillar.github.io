a1d879d023acc84df7efcacf02e314eab1b03153
EdgarAguillar <123840623+EdgarAguillar@users.noreply.github.com>
2024-03-21 23:14:14 +0000

    cambios de conexion

--- Advance/Modelo/Conexion.php
+++ Advance/Modelo/Conexion.php
@@ -2,7 +2,7 @@
 $dbHost='localhost';
 $dbName='integradora';
 $dbUser='root';
-$dbPass='';
+$dbPass='root';
 try{
     $pdo=new PDO("mysql:host=$dbHost;dbname=$dbName",$dbUser,$dbPass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
