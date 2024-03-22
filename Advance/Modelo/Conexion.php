7e7f2c2244283bec462a82b70baca09e234064b2
EdgarAguillar <123840623+EdgarAguillar@users.noreply.github.com>
2024-03-22 00:14:14 +0000

    dadn deiondeondine

--- Advance/Modelo/Conexion.php
+++ Advance/Modelo/Conexion.php
@@ -1,13 +1,12 @@
 <?php
-$dbHost = 'localhost';
-$dbName = 'Integradora';
-$dbUser = 'root';
-$dbPass = 'root';
-
-try {
-    $pdo = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Error de conexión: " . $e->getMessage();
+$dbHost='localhost';
+$dbName='Integradora';
+$dbUser='root';
+$dbPass='root';
+try{
+    $pdo=new PDO("mysql:host=$dbHost;dbname=$dbName",$dbUser,$dbPass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    }catch (Exception $e){
+    echo $e->getMessage();
 }
 ?>
